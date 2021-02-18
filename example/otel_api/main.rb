cb62def03adf05ce73ca5a814d4bd23f56e6253b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-02-18 13:52:02 +0200

    Add Otel API example

---
+++ example/otel_api/main.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
+require 'uptrace'
+
+tracer = OpenTelemetry.tracer_provider.tracer('my_app_or_gem', '0.1.0')
+
+# Start a span and set some attributes.
+
+tracer.in_span('main') do |span|
+  span.set_attribute('key1', 'value1')
+  span.set_attribute('key1', 123.456)
+
+  span.add_event(
+    name: 'log',
+    attributes: {
+      'log.severity': 'error',
+      'log.message': 'User not found',
+      'enduser.id': '123'
+    }
+  )
+
+  span.record_exception(ArgumentError.new('error1'))
+
+  span.status = OpenTelemetry::Trace::Status.new(
+    OpenTelemetry::Trace::Status::ERROR,
+    description: 'error description'
+  )
+end
+
+# Active span logic.
+puts('------------------------------------------------------------')
+
+tracer.in_span('main') do |main|
+  raise ArgumentError, 'not reached' unless OpenTelemetry::Trace.current_span == main
+
+  puts('main is active')
+
+  tracer.in_span('main') do |child|
+    raise ArgumentError, 'not reached' unless OpenTelemetry::Trace.current_span == child
+
+    puts('child is active')
+  end
+
+  raise ArgumentError, 'not reached' unless OpenTelemetry::Trace.current_span == main
+
+  puts('main is active again')
+end
+
+# Start a span and activate it manually. Don't forget to finish the span.
+puts('------------------------------------------------------------')
+
+main = tracer.start_span(
+  'main',
+  kind: OpenTelemetry::Trace::SpanKind::SERVER,
+  attributes: {
+    foo: 'bar'
+  }
+)
+
+OpenTelemetry::Trace.with_span(main) do |span|
+  puts(span == main)
+end
+
+main.finish
